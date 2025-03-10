53aca4e456753b43f6133de20ee772309f9e2538
messiah-porter <messiah.porter@stu.barren.kyschools.us>
2025-03-10 10:45:17 -0500

    bonus done

--- script.js
+++ script.js
@@ -48,4 +48,24 @@ const {blackwidow: Nat, HawkEye: others, ironMan: other} = moreAvengers
 
 console.log(moreAvengers);
 
+//BONUS
+
+//BONUS
+
+//BONUS
+
+const bonus = {
+    first: [1, 2, 3],
+    second: [4, 5, 6],
+    third: [7, 8, 9]
+  };
+
+ const all = {
+    ...bonus
+}
+
+ console.log(all);
  
+ const {1: one, 2: two, 3: three, 4: four, 5: five, 6: six, 7: seven, 8: eight, 9: nine} = bonus;
+
+ console.log(all);
